0c0ca58876b04346b2a530d28707e8803c228f1c
theocarpes <70979960+theocarpes@users.noreply.github.com>
2025-09-08 07:56:29 -0300

    Add files via upload

---
+++ imersao1/dados.js
@@ -0,0 +1,53 @@
+
+let dados = [
+    {
+        titulo: "Luiz Rauber",
+        descricao: "Luiz Rauber é o professor de técnico no Senac Santa Cruz. Depende.",
+        tags: "tecnico técnico"
+    },
+    {
+        titulo: "Carlos",
+        descricao: "Carlos é o professor de sociologia e PV. Geralmente é bem deboa mas quando atrapalham a sua aula fica bem bravo.",
+        tags: "sociologia pv projeto de vida"
+    },
+    {
+        titulo: "João",
+        descricao: "João é o professor de Educação Fisica e PI. Nas aulas de Ed. Fisica sempre diz que quanto mais conversamos menos tempo de jogar vamos ter.",
+        tags: "educação fisica PI ed fisica"
+    },
+    {
+        titulo: "Eduardo",
+        descricao: "Eduardo é o professor de Inglês. Bem tranquilo mas fica puto se atrapalharem a aula.",
+        tags: "ingles inglês",
+    },
+    {
+        titulo: "Livia",
+        descricao: "Livia é a professora de matematica e tambem era de física.",
+        tags: "mat matematica",
+    },
+    {
+        titulo: "Bruna",
+        descricao: "Bruna é a professora de história, geografia e filosofia.",
+        tags: " filo filosofia historia geo geografia",
+    },
+    {
+        titulo: "Poliana",
+        descricao: "Poliana é a professora de biologia. Começou a nos dar aula a pouco tempo mas parece gostar da nossa turma.",
+        tags: "bio biologia",
+    },
+    {
+        titulo: "Carla",
+        descricao: "Carla é a professora de português. Odeia nossa turma.",
+        tags: "port portugues português",
+    },
+    {
+        titulo: "Natalia",
+        descricao: "Natalia é a nova professora de Física.  ",
+        tags: "Física fis",
+    },
+    {
+        titulo: "Alana",
+        descricao: "Alana é a nova professora de artes. Bem tranquila e parece gostar da nossa turma",
+        tags: "Artes",
+    },
+];
